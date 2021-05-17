a5e829fca7d206ec1e7f58fdbe6e2f2c9a396d63
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2021-05-18 00:22:42 +0200

    history system

---
+++ Assets/Scripts/History.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class History
+{
+
+
+	public const string k_file_history_key = "file_history";
+
+
+	public static void Update ( string path )
+	{
+		List<string> history = new List<string>( Read() );
+		int pos = history.IndexOf(path);
+		if( pos!=0 )
+		{
+			if( pos!=-1 ) history.RemoveAt( pos );
+			history.Insert( 0 , path );
+			Write( history.ToArray() );
+		}
+	}
+
+	public static string[] Read ()
+	{
+		string json = PlayerPrefs.GetString( k_file_history_key , JsonUtility.ToJson( new HistoryData{ Values=new string[0] } ) );
+		return JsonUtility.FromJson<HistoryData>(json).Values;
+	}
+
+	public static void Write ( string[] array )
+	{
+		string json = JsonUtility.ToJson( new HistoryData{ Values=array } );
+		PlayerPrefs.SetString( k_file_history_key , json );
+	}
+
+
+	[System.Serializable]
+	public struct HistoryData
+	{
+		public string[] Values;
+	}
+
+
+}
---
+++ Assets/Scripts/HistoryListView.cs
@@ -0,0 +1,57 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+using IO = System.IO;
+
+public class HistoryListView : VisualElement
+{
+
+	public event System.Action<string> onClicked = (s) => {};
+
+	public new class UxmlFactory : UxmlFactory<HistoryListView,UxmlTraits> {}
+	public new class UxmlTraits : VisualElement.UxmlTraits
+	{
+		public override void Init ( VisualElement ve , IUxmlAttributes bag , CreationContext cc )
+		{
+			base.Init( ve , bag , cc );
+			HistoryListView ROOT = ve as HistoryListView;
+
+			string[] items = History.Read();
+			#if UNITY_EDITOR
+			if( items.Length==0 ) items = new string[]{ "lorem ipsum" , "lorem ipsum" , "lorem ipsum" , "lorem ipsum" };
+			#endif
+
+			var LISTVIEW = new ListView();
+			LISTVIEW.style.flexGrow = 1;
+			{
+				LISTVIEW.itemHeight = 20;
+				LISTVIEW.itemsSource = items;
+				LISTVIEW.makeItem = () => new Label();
+				LISTVIEW.bindItem = (item,i) =>
+				{
+					string path = (string) LISTVIEW.itemsSource[i];
+					Label label = (Label) item;
+					label.text = path;
+					label.SetEnabled( IO.File.Exists(path) );
+				};
+
+				// on selection change:
+				LISTVIEW.onSelectionChange += (obj)=>
+				{
+					string path = (string) obj.FirstOrDefault();
+					ROOT.onClicked( path );
+				};
+			}
+			
+			ROOT.Add( LISTVIEW );
+			{
+				var style = ROOT.style;
+				style.minHeight = LISTVIEW.itemHeight * 4.5f;
+			}
+		}
+
+	}
+	
+
+}
